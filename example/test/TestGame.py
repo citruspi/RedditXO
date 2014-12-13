2a0ea5750b614b34e2402e2b0660708bc44ea5dd
kocsen <kocsenc@gmail.com>
2014-12-13 13:36:02 -0500

    deploying

--- example/test/TestGame.py
+++ example/test/TestGame.py
@@ -1,112 +1,29 @@
-#!/usr/bin/python
-import pygame
-from gi.repository import Gtk
+"""
+ZetCode PyQt4 tutorial 
+
+In this example, we create a simple
+window in PyQt4.
+
+author: Jan Bodnar
+website: zetcode.com 
+last edited: October 2011
+"""
+
+import sys
+from PyQt4 import QtGui
 
 
-class TestGame:
-    def __init__(self):
-        # Set up a clock for managing the frame rate.
-        self.clock = pygame.time.Clock()
-
-        self.x = -100
-        self.y = 100
-
-        self.vx = 10
-        self.vy = 0
-
-        # use a (r, g, b) tuple for color
-        self.red = (255, 0, 0)
-        self.green = (0, 255, 0)
-        self.blue = (0, 0, 255)
-        self.yellow = (255, 255, 0)
-
-        self.subreddits = {
-            'Science': 'science',
-            'Technology': 'technology',
-            'World News': 'news',
-            'Money': 'money'
-        }
-
-        self.paused = False
-        self.direction = 1
-
-    def set_paused(self, paused):
-        self.paused = paused
-
-    # Called to save the state of the game to the Journal.
-    def write_file(self, file_path):
-        pass
-
-    # Called to load the state of the game from the Journal.
-    def read_file(self, file_path):
-        pass
-
-    # The main game loop.
-    def run(self):
-        self.running = True
-
-        screen = pygame.display.get_surface()
-        self.screen = screen
-        label_arr = []
-
-        while self.running:
-
-            # Pump GTK messages.
-            while Gtk.events_pending():
-                Gtk.main_iteration()
-
-            # Pump PyGame messages.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    return
-                elif event.type == pygame.VIDEORESIZE:
-                    pygame.display.set_mode(event.size, pygame.RESIZABLE)
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT:
-                        self.direction = -1
-                    elif event.key == pygame.K_RIGHT:
-                        self.direction = 1
-                elif event.type == pygame.MOUSEBUTTONUP:
-                    pos = pygame.mouse.get_pos()
-                    self.printText(str(pos.__str__()), 50, 90, 500, self.blue)
-
-            # Print Title
-            self.printText("RedditXO Client", 90, 450, 50, self.yellow)
-
-            start_x_pos_subreddits = 150
-            start_y_pos_subreddits = 150
-
-            for key in self.subreddits:
-                label_arr.append(self.printText(key, 60, start_x_pos_subreddits, start_y_pos_subreddits, self.green))
-                start_y_pos_subreddits += 100
-
-
-            # Flip Display
-            #pygame.display.flip()
-            # fill later
-
-            # Try to stay at 30 FPS
-            self.clock.tick(30)
-
-    def printText(self, txtText, Textsize, Textx, Texty, Textcolor, text_font="MS Comic Sans"):
-        # pick a font you have and set its size
-        myfont = pygame.font.SysFont(text_font, Textsize)
-        # apply it to text on a label
-        label = myfont.render(txtText, 1, Textcolor)
-        # put the label object on the screen at point Textx, Texty
-        self.screen.blit(label, (Textx, Texty))
-        # show the whole thing
-        pygame.display.flip()
-        return label
-
-
-# This function is called when the game is run directly from the command line:
-# ./TestGame.py
 def main():
-    pygame.init()
-    pygame.display.set_mode((0, 0), pygame.RESIZABLE)
-    game = TestGame()
-    game.run()
+    
+    app = QtGui.QApplication(sys.argv)
+
+    w = QtGui.QWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('RedditXO')
+    w.show()
+    
+    sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
