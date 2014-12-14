d66646c8679506e1c0d0142a834d6bece13ed51d
Mihir Singh <git.service@mihirsingh.com>
2014-12-14 12:03:58 -0500

    Added a method for fetching the hottest ten posts in group

--- example/test/reddit.py
+++ example/test/reddit.py
@@ -12,3 +12,20 @@ class Client(object):
         connection = praw.Reddit(useragent)
 
         self.connection = connection
+
+    def fetchgroup(self, group):
+
+        groups = {
+                'science': ['science', 'askscience'],
+                'technology': ['technology'],
+                'news': ['news', 'worldnews', 'inthenews'],
+                'sports': ['sports'],
+                'travel': ['travel'],
+                'math': ['math', 'learnmath', 'cheatatmathhomework']
+        }
+
+        subreddits = '+'.join(groups[group])
+
+        subreddit = self.connection.get_subreddit(subreddits)
+
+        return subreddit.get_hot(limit=10)
